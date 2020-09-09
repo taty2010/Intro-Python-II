8c0831b64141655a742d3933421ab3af195c550d
taty2010 <shydalys123@gmail.com>
2020-09-08 23:00:53 -0500

    set classes

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -49,3 +49,11 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+name = input("Please enter your name: ")
+r = input("Choose a room (): ")
+
+
+p1 = Player('Tatyana', 'outside')
+print(p1.name)
+print(p1.currentRoom)
--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from room import Room
+
+class Player:
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
+        self.prevRoom = []
+        def previous_rooms(self, room):
+            history = Room(room, self)
+            self.prevRoom.append(history)
